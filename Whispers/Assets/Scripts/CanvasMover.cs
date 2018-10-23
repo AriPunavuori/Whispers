0090857480acf4fc6a64d646d4b8457be0a5ac38
Nikl3 <niko.tuuli@hotmail.com>
2018-10-23 16:03:08 +0300

    Network connection structure phase 3/4 from tutorial

---
+++ Whispers/Assets/Scripts/CanvasMover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasMover : MonoBehaviour {
+
+    Canvas canvas;
+
+    void Start() {
+        canvas = FindObjectOfType<Canvas>();
+        transform.parent = canvas.transform;
+    }
+	
+	void Update () {
+		
+	}
+}
