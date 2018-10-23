bad49b128de4170e5e18cd15f7a47f299026b5b3
Ari Punavuori <ari.punavuori@gmail.com>
2018-10-23 16:24:26 +0300

    Blurred Lines

--- Whispers/Assets/DrawingMachine.cs
+++ Whispers/Assets/DrawingMachine.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DrawingMachine : MonoBehaviour {
+    public static DrawingMachine instance;
+    public List<List<Vector3>> lines;
 
+    private void Awake() {
+        lines = new List<List<Vector3>>();
+        instance = this;
+    }
 
-
-
-	void Update () {
-        if(InputManager.instance.drawing){
+    void Update () {
+        if(InputManager.instance.IsDrawing()){
 
         }
 	}
+
+
+
 }
--- Whispers/Assets/InputManager.cs
+++ Whispers/Assets/InputManager.cs
@@ -6,56 +6,72 @@ public class InputManager : MonoBehaviour {
 
     Vector3 curPos;
     public Vector3 drawPos;
-    public bool drawing = false;
-    bool drawingEnded = false;
- 
-    float lineDotThreshold = 1;
+    List<Vector3> line;
+    LineRenderer currentLine;
+    public GameObject linePrefab;
+    float lineDotThreshold = 0;
+
+    public bool IsDrawing() {
+        return currentLine != null;
+    }
 
     public static InputManager instance;
 
+
+
     private void Awake() {
         instance = this;
+        line = new List<Vector3>();
     }
 
     void Update () {
-
-        if(drawingEnded){
-            drawing = false;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Mouse0)){
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if(Physics.Raycast(ray)){
-                curPos = Input.mousePosition;
-                drawPos = Input.mousePosition;
-                drawing = true;
-                drawingEnded = false;
-                // Viivan ensimmäinen piste on drawPos
-            }
+        /*if(Input.GetKeyDown(KeyCode.Space)) {
+            var lr = GetComponent<LineRenderer>();
+            lr.positionCount = 3;
+            var pos = new Vector3[] {
+                Vector3.zero,
+                new Vector3(1,1,0),
+                new Vector3(1,0,0)
+            };
+            lr.SetPositions(pos);
+        }*/
+            if(Input.GetKeyDown(KeyCode.Mouse0)){
+            curPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            curPos.z = 0;
+            drawPos = curPos;
+            var newLine  = Instantiate(linePrefab);
+            currentLine = newLine.GetComponent<LineRenderer>();
+            // Viivan ensimmäinen piste on drawPos
+            line.Clear();
+            line.Add(drawPos);
+            currentLine.positionCount = 0;
+            print("Viiva alkaa");
         }
 
         if(Input.GetKey(KeyCode.Mouse0)) {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if(Physics.Raycast(ray)) {
-                curPos = Input.mousePosition;
-                if(Vector3.Distance(curPos, drawPos) > lineDotThreshold){
-                    drawPos = curPos;
-                    // Viivan n:s piste on on drawPos
-                }
-            }
+            curPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            curPos.z = 0;
+            //if(Vector3.Distance(curPos, drawPos) > lineDotThreshold){
+                drawPos = curPos;
+                line.Add(drawPos);
+                currentLine.positionCount = line.Count;
+                currentLine.SetPositions(line.ToArray());
+                // Viivan n:s piste on on drawPos
+            //}
+            print("Viiva jatkuu");
         }
 
         if (Input.GetKeyUp(KeyCode.Mouse0)){
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if(Physics.Raycast(ray)){
-                curPos = Input.mousePosition;
-                if(Vector3.Distance(curPos,drawPos) > lineDotThreshold){
-                    drawPos = curPos;
-                }
-                // Viivan päätepiste on drawPos;
-            }
-            drawingEnded = true;
+            curPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            curPos.z = 0;
+            //if(Vector3.Distance(curPos,drawPos) > lineDotThreshold){
+                drawPos = curPos;
+            //}
+            line.Add(drawPos);
+            currentLine.positionCount = line.Count;
+            currentLine.SetPositions(line.ToArray());
+            DrawingMachine.instance.lines.Add(line);
+            print("Viiva loppuu");
         }
-
     }
 }
