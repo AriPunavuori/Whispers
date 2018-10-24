eca7eb0a8ddb7e6eedc1dbd24c6e6e42d6947aa1
Ari Punavuori <ari.punavuori@gmail.com>
2018-10-24 14:44:12 +0300

    Draing erasing and touchcontrols

--- Whispers/Assets/DrawingMachine.cs
+++ Whispers/Assets/DrawingMachine.cs
@@ -3,20 +3,89 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DrawingMachine : MonoBehaviour {
+    public enum Drawmode { Draw, Erase };
     public static DrawingMachine instance;
     public List<List<Vector3>> lines;
+    public Vector3 drawPos;
+    List<Vector3> line;
+    LineRenderer currentLine;
+    public GameObject linePrefab;
+    public GameObject eraserPrefab;
+    int lineNumber = 0 ;
+    float lineDotThreshold = 0.1f;
+    public Drawmode mode;
 
     private void Awake() {
         lines = new List<List<Vector3>>();
+        line = new List<Vector3>();
         instance = this;
+        mode = Drawmode.Draw;
     }
 
-    void Update () {
-        if(InputManager.instance.IsDrawing()){
 
+    public void PrintLines(){
+        //print(lines.Count);
+        //for(int i = 0 ; i < lines.Count ; i++){
+        //    var s = "";
+        //    for(int j = 0 ; j < lines[i].Count ; j++){
+        //        s += lines[i][j] + " ";
+        //    }
+        //    print(s);
+        //}
+        foreach (var l in lines) {
+            var s = "";
+            foreach (var coord in l) {
+                s += coord + " ";
+            }
+            print(s);
         }
-	}
+    }
 
+    public void LineStart(Vector3 curPos) {
+        drawPos = curPos;
 
+        if (mode == Drawmode.Draw){
+            var newLine = Instantiate(linePrefab);
+            currentLine = newLine.GetComponent<LineRenderer>();
+        } else {
+            var newLine = Instantiate(eraserPrefab);
+            currentLine = newLine.GetComponent<LineRenderer>();
+        }
+
+        line.Clear();
+        line.Add(drawPos);
+        currentLine.sortingOrder = lineNumber;
+        lineNumber++;
+        currentLine.positionCount = 0;
+    }
+
+    public void LineContinued(Vector3 curPos){
+        if(Vector3.Distance(curPos, drawPos) > lineDotThreshold) {
+            drawPos = curPos;
+            line.Add(drawPos);
+            currentLine.positionCount = line.Count;
+            currentLine.SetPositions(line.ToArray());
+        }
+    }
+
+    public void LineEnded(Vector3 curPos) {
+        if(Vector3.Distance(curPos, drawPos) > lineDotThreshold) {
+            drawPos = curPos;
+        }
+        line.Add(drawPos);
+
+        currentLine.positionCount = line.Count;
+        currentLine.SetPositions(line.ToArray());
+        DrawingMachine.instance.lines.Add(new List<Vector3>(line));
+        DrawingMachine.instance.PrintLines();
+    }
+
+    public void DrawingEnabler(){
+        mode = Drawmode.Draw;
+    }
+
+    public void EraserEnabler(){
+        mode = Drawmode.Erase;
+    }
 
 }
--- Whispers/Assets/InputManager.cs
+++ Whispers/Assets/InputManager.cs
@@ -4,74 +4,40 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour {
 
-    Vector3 curPos;
-    public Vector3 drawPos;
-    List<Vector3> line;
-    LineRenderer currentLine;
-    public GameObject linePrefab;
-    float lineDotThreshold = 0;
-
-    public bool IsDrawing() {
-        return currentLine != null;
-    }
-
     public static InputManager instance;
-
-
 
     private void Awake() {
         instance = this;
-        line = new List<Vector3>();
     }
 
-    void Update () {
-        /*if(Input.GetKeyDown(KeyCode.Space)) {
-            var lr = GetComponent<LineRenderer>();
-            lr.positionCount = 3;
-            var pos = new Vector3[] {
-                Vector3.zero,
-                new Vector3(1,1,0),
-                new Vector3(1,0,0)
-            };
-            lr.SetPositions(pos);
-        }*/
-            if(Input.GetKeyDown(KeyCode.Mouse0)){
-            curPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            curPos.z = 0;
-            drawPos = curPos;
-            var newLine  = Instantiate(linePrefab);
-            currentLine = newLine.GetComponent<LineRenderer>();
-            // Viivan ensimmäinen piste on drawPos
-            line.Clear();
-            line.Add(drawPos);
-            currentLine.positionCount = 0;
-            print("Viiva alkaa");
+    void Update() {
+
+        if(Input.GetKeyDown(KeyCode.Mouse0)|| Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) {
+            var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            pos.z = 0;
+            DrawingMachine.instance.LineStart(pos);
         }
 
-        if(Input.GetKey(KeyCode.Mouse0)) {
-            curPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            curPos.z = 0;
-            //if(Vector3.Distance(curPos, drawPos) > lineDotThreshold){
-                drawPos = curPos;
-                line.Add(drawPos);
-                currentLine.positionCount = line.Count;
-                currentLine.SetPositions(line.ToArray());
-                // Viivan n:s piste on on drawPos
-            //}
-            print("Viiva jatkuu");
+        if(Input.GetKey(KeyCode.Mouse0)|| Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) {
+            var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            pos.z = 0;
+            DrawingMachine.instance.LineContinued(pos);
         }
 
-        if (Input.GetKeyUp(KeyCode.Mouse0)){
-            curPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            curPos.z = 0;
-            //if(Vector3.Distance(curPos,drawPos) > lineDotThreshold){
-                drawPos = curPos;
-            //}
-            line.Add(drawPos);
-            currentLine.positionCount = line.Count;
-            currentLine.SetPositions(line.ToArray());
-            DrawingMachine.instance.lines.Add(line);
-            print("Viiva loppuu");
+        if (Input.GetKeyUp(KeyCode.Mouse0)|| Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) {
+            var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            pos.z = 0;
+            DrawingMachine.instance.LineEnded(pos);
         }
+
+        if (Input.GetKeyDown(KeyCode.Space)){
+            if(DrawingMachine.instance.mode == DrawingMachine.Drawmode.Draw){
+                DrawingMachine.instance.DrawingEnabler();
+            } else {
+                DrawingMachine.instance.EraserEnabler();
+            }
+            
+        }
+
     }
 }
