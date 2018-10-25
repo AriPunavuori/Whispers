a10ca1429ea249f05530007f2cfcbe2d297f57f1
Nikl3 <niko.tuuli@hotmail.com>
2018-10-25 11:07:17 +0300

    roundatamanagerr

---
+++ Whispers/Assets/RoundDataManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundDataManager : MonoBehaviour {
+
+    [System.Serializable]
+    public struct RoundData {
+        
+}
+
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
