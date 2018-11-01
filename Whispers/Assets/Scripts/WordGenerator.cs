8d9e1d0df1e4fc6845d587b22d9c2ee35ce55df3
Nikl3 <niko.tuuli@hotmail.com>
2018-11-01 10:47:44 +0200

    wordgenerator word fix

--- Whispers/Assets/Scripts/WordGenerator.cs
+++ Whispers/Assets/Scripts/WordGenerator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class WordGenerator : MonoBehaviour {
     public static WordGenerator instance;
     public TextAsset adj;
@@ -21,14 +22,15 @@ public class WordGenerator : MonoBehaviour {
     }
 
     void Start() {
-        adjList = adj.text.Split("\n"[0]);
-        nounList = noun.text.Split("\n"[0]);
+        adjList = adj.text.Split((string[])null, System.StringSplitOptions.RemoveEmptyEntries);
+        nounList = noun.text.Split((string[])null, System.StringSplitOptions.RemoveEmptyEntries);
     }
 
     public void WordG() { // Sanageneraattori
         Adj = (adjList[Random.Range(0, adjList.Length)]);
         Noun = (nounList[Random.Range(0, nounList.Length)]);
         myWord = Adj + " " + Noun;
+        print(Adj.Length);
         GameManager.instance.ChangeUIText("Hi " + PlayerManager.instance.playerData.playerName + ", Can you draw:\n" + myWord + "?");
     }
 }
