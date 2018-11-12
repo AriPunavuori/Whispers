6cf458f470b394fb6ceeeab6da2a129c5508cf69
Ose Hermunen <ozzah7@hotmail.com>
2018-11-12 15:16:22 +0200

    endscreenstuff.

--- Whispers/Assets/Scripts/EndScreen.cs
+++ Whispers/Assets/Scripts/EndScreen.cs
@@ -1,10 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndScreen : MonoBehaviour {
 
+    public Camera eCam;
+
+    public GameObject picPrefab;
+    public Transform origPic;
+
     RoundDataManager rdm;
+
+    public Text GuessText;
+
+    public Vector3 picPos;
 
 	// Use this for initialization
 	void Start () {
@@ -18,15 +28,20 @@ public class EndScreen : MonoBehaviour {
         //}
         var ch = rdm.chains[0];
         int round = 0;
+        picPos = new Vector3(origPic.position.x, origPic.position.y + (-650 * round), origPic.position.z);
         while (true) {
             if (ch.guesses.Count <= round)
                 break;
+            Instantiate(picPrefab, picPos, Quaternion.identity, origPic);
             // do something - ch.guesses[round]
-            //ch.guesses[0];
+            //SMTalk.text = ch.guesses[round];
             round++;
             if (ch.pictures.Count <= round)
                 break;
+            Instantiate(picPrefab, picPos, Quaternion.identity, origPic);
+
             // do something - ch.pictures[round]
+
             //ch.pictures[1];
             round++;
         }
