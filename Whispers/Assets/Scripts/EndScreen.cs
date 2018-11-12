42b567948d04c32b438362b446c8aa38b70c26cc
Ose Hermunen <ozzah7@hotmail.com>
2018-11-12 13:56:52 +0200

    endscreen started

---
+++ Whispers/Assets/Scripts/EndScreen.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndScreen : MonoBehaviour {
+
+    RoundDataManager rdm;
+
+	// Use this for initialization
+	void Start () {
+        rdm = GameObject.FindObjectOfType<RoundDataManager>();
+	}
+	
+
+    void ThreadDisplay() {
+        //foreach (var data in rdm.chains) {
+        //    foreach (var pic in 
+        //}
+        var ch = rdm.chains[0];
+        int round = 0;
+        while (true) {
+            if (ch.guesses.Count <= round)
+                break;
+            // do something - ch.guesses[round]
+            //ch.guesses[0];
+            round++;
+            if (ch.pictures.Count <= round)
+                break;
+            // do something - ch.pictures[round]
+            //ch.pictures[1];
+            round++;
+        }
+
+    }
+
+
+}
