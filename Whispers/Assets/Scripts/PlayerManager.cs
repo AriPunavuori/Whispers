a04db6217938206cc412ca0d6ef29a5191a6cbd0
Ari Punavuori <ari.punavuori@gmail.com>
2018-11-22 13:05:48 +0200

    .

--- Whispers/Assets/Scripts/PlayerManager.cs
+++ Whispers/Assets/Scripts/PlayerManager.cs
@@ -7,11 +7,11 @@ using UnityEngine.UI;
 public struct PlayerData {
     public string playerName;
     public bool playerRDY;
-    public List<float> playerIMG;
+    public float playerIMG;
     public bool playerIsHost;
     public int playerID;
 
-    public PlayerData(string playerName, bool playerRDY, List<float> playerIMG, bool playerIsHost, int playerID) {
+    public PlayerData(string playerName, bool playerRDY, float playerIMG, bool playerIsHost, int playerID) {
         this.playerName = playerName;
         this.playerRDY = playerRDY;
         this.playerIMG = playerIMG;
