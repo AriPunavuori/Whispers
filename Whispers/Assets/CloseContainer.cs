918236e0df281c8bf7b396730538514b4d015ae5
Nikl3 <niko.tuuli@hotmail.com>
2018-11-27 13:35:54 +0200

    MenuScene animation fixes

---
+++ Whispers/Assets/CloseContainer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseContainer : MonoBehaviour {
+
+	public void CloseCont() {
+        FindObjectOfType<PlayerManager>().textContainer.SetActive(false);
+    }
+}
