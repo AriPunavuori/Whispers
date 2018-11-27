078af193003dde8b7f8abb896fb9cb94f6f855a7
Ari Punavuori <ari.punavuori@gmail.com>
2018-11-27 11:37:37 +0200

    Prvious buttom

--- Whispers/Assets/Scripts/DemoStyleEnd.cs
+++ Whispers/Assets/Scripts/DemoStyleEnd.cs
@@ -6,15 +6,15 @@ using TMPro;
 
 public class DemoStyleEnd : MonoBehaviour {
 
-    public int round = 0;
-    public int chain = 0;
+    int round = -1;
+    int chain = 0;
 
     RoundDataManager rdm;
 
     public TextMeshProUGUI guessText;
 
     HostGame hg;
-
+     
     GameManager gm;
 
     PlayerManager pm;
@@ -32,26 +32,21 @@ public class DemoStyleEnd : MonoBehaviour {
 
 
     public void Next() {
-        print("nappi painettu");
         um.PocketReset();
+        round++;
         //guessText.text = "";
         if (hg.numberOfPlayers < round) {
             chain++;
-            print("Ketju vaihtuu");
             round = 0;
             guessText.text = "Next chain of events looks like this:";
             if(chain >= hg.numberOfPlayers) {
                 chain = 0; //Teksti thats it folks ja nappi play again?
             }
         }
-        print("Cahin: " + chain);
-        print("ROund#" + round);
 
         var ch = rdm.chains[chain];
 
         if (round % 2 == 0) {
-            print("Parillinen");
-            print("teksti ja Rundi: " + round);
             if (round == 0) {
                 guessText.text = pm.playerDataList[((chain - 1 + hg.numberOfPlayers) % hg.numberOfPlayers)].playerName + " was asked to draw " + ch.guesses[0];
             } else {
@@ -61,8 +56,6 @@ public class DemoStyleEnd : MonoBehaviour {
 
 
         } else {
-            print("Pariton");
-            print("Kuva ja roundinumero: " + round);
             var pics = rdm.chains[chain].pictures;
             if (round - 1 == 0) {
                 guessText.text = "to which this " + pm.playerDataList[((chain - 1 + hg.numberOfPlayers) % hg.numberOfPlayers)].playerName + " drew as";
@@ -72,15 +65,43 @@ public class DemoStyleEnd : MonoBehaviour {
                 um.ShowPicture(pics[(round - 1) / 2]);
             }
         }
-        round++;
+    }
+
+    public void Previous() {
+        round--;
+        um.PocketReset();
+        //guessText.text = "";
+
+        if(round < 0) {
+            chain--;
+            round = hg.numberOfPlayers;
+            //guessText.text = "Next chain of events looks like this:";
+            if(chain < 0 ) {
+                chain = hg.numberOfPlayers - 1; //Teksti thats it folks ja nappi play again?
+            }
+        }
+
+        var ch = rdm.chains[chain];
+
+        if(round % 2 == 0) {
+
+            if(round == 0) {
+                guessText.text = pm.playerDataList[((chain - 1 + hg.numberOfPlayers) % hg.numberOfPlayers)].playerName + " was asked to draw " + ch.guesses[0];
+            } else {
+                guessText.text = "Which " + pm.playerDataList[(chain - round + hg.numberOfPlayers) % hg.numberOfPlayers].playerName + " deciphered as:\n " + ch.guesses[round / 2];
+            }
+            // (chain % hg.numberOfPlayers - 1)
+
+
+        } else {
+            var pics = rdm.chains[chain].pictures;
+            if(round - 1 == 0) {
+                guessText.text = "to which this " + pm.playerDataList[((chain - 1 + hg.numberOfPlayers) % hg.numberOfPlayers)].playerName + " drew as";
+                um.ShowPicture(pics[0]);
+            } else {
+                guessText.text = "which " + pm.playerDataList[(chain - round + hg.numberOfPlayers) % hg.numberOfPlayers].playerName + " drew as";
+                um.ShowPicture(pics[(round - 1) / 2]);
+            }
+        }
     }
 }
-
-
-
-//    public void Previous() {
-//        if (round % 1 == 0) ;
-
-//    }
-
-//}
