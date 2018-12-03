9365695bbdb200c40e7ff0a273407a881b1d0b12
Nikl3 <niko.tuuli@hotmail.com>
2018-12-03 13:08:10 +0200

    Some more app start animations

---
+++ Whispers/Assets/Scripts/ToggleGameObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleGameObject : MonoBehaviour {
+
+    public GameObject NameSetContainer;
+
+    public void ShowCont() {
+        print("testi");
+        NameSetContainer.SetActive(true);
+    }
+    
+    public void HideCont() {
+        NameSetContainer.SetActive(false);
+    }
+
+}
